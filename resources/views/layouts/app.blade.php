4890fc7695e627409aca3208d6cc71bd90dafaba
booming24 <irfanthq04@gmail.com>
2024-08-02 23:16:02 +0700

    fix file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>laravel</title>
+    <title>Tes 123</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
